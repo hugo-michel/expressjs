a0b65a1f49d42a9f29bd823f55762c72870c6935
hugo-michel <orayito62@gmail.com>
2023-09-25 16:29:55 +0200

    amelioration de la homepage

--- app.js
+++ app.js
@@ -3,9 +3,22 @@ const app = express()
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    let html = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello world</title>
+</head>
+<body>
+    <h1>Hello world !</h1>
+</body>
+</html>
+    `;
+
+    res.send(html)
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
